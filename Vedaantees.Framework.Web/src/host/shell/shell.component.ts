9a2c0039f745835b52d964e9635c4a1860edd80c
Abhay Naik <vedaantees@gmail.com>
2018-05-10 17:14:02 +0530

    ui changes

--- Vedaantees.Framework.Web/src/host/shell/shell.component.ts
+++ Vedaantees.Framework.Web/src/host/shell/shell.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
 })
 export class ShellComponent 
 {
-  title = 'Hello World - 1';
+    moduleName:string;
+
+    constructor()
+    {
+      this.moduleName = "Aware";
+    }
 }
